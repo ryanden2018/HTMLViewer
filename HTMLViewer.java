f2e33c83b88ddab232b11f356e3c9ad9fac6e483
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-27 09:17:07 -0500

    added monospaced font

--- HTMLViewer.java
+++ HTMLViewer.java
@@ -10,12 +10,15 @@ class HTMLViewer {
   JTree jtree;
   JScrollPane jscroll;
   DefaultMutableTreeNode top;
+  Font font;
 
   HTMLViewer() {
     jfrm = new JFrame("HTMLViewer");
     top = new DefaultMutableTreeNode("<html>&lt;html&gt;</html>");
     jtree = new JTree(top);
     jscroll = new JScrollPane(jtree);
+    font = new Font("monospaced",Font.PLAIN,16);
+    jtree.setFont(font);
 
     jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     jfrm.add(jscroll,BorderLayout.CENTER);
@@ -26,14 +29,16 @@ class HTMLViewer {
   public static void main(String[] args) {
     HTMLParser parser = new HTMLParser();
 
-   // String docu = "<!DOCTYPE html><html lang='en'><head><meta charset='utf-8' /><title>A title</title><script>var s = ' \' </head> \' ';alert(s);</script><script src='test.js' /></head><body><!-- some comment with <b> html inside some comment with <b> html inside some comment with <b> html inside some comment with <b> html inside --><h1>A header</h1><b><i>something</b> something </i> else<div><div></div><div></div></div><div></div><div><p>Some text in a <em>paragraph</em><p>Matthew's PB and J sandwich<br>hi there!<br><p><a href='someplace.htm'>a link</a> in a p tag</div><ul><li>thing one<li>thing two<li>thing three</ul><script src='something.js'></script></body></html>";
+    String docu = "<!DOCTYPE html><html lang='en'><head><meta charset='utf-8' /><title>A title</title><script>var s = ' \' </head> \' ';alert(s);</script><script src='test.js' /></head><body><!-- some comment with <b> html inside some comment with <b> html inside some comment with <b> html inside some comment with <b> html inside --><h1>A header</h1><b><i>something</b> something </i> else<div><div></div><div></div></div><div></div><div><p>Some text in a <em>paragraph</em><p>Matthew's PB and J sandwich<br>hi there!<br><p><a href='someplace.htm'>a link</a> in a p tag</div><ul><li>thing one<li>thing two<li>thing three</ul><script src='something.js'></script></body></html>";
 
-    String docu = "";
-    try {
-      docu = new String ( Files.readAllBytes( Paths.get("airbnb.html")) );
-    } catch (java.io.IOException e) {
-      e.printStackTrace();
-    }
+    // String docu = "";
+    // try {
+    //   docu = new String ( Files.readAllBytes( Paths.get("Google.htm")) );
+    // } catch (java.io.IOException e) {
+    //   e.printStackTrace();
+    // }
+    
+    
     parser.parse(docu);
 
     (new HTMLViewer()).addNodes(parser);   
